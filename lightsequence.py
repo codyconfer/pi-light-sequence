13805c1a832959e9ecd58d36da6e46d59611c445
codyconfer <mail@codyconfer.me>
2019-07-07 01:06:32 -0600

    clean up

--- lightsequence.py
+++ lightsequence.py
@@ -12,33 +12,33 @@ class Sequence:
 
 
 class Grid:
+    def __init__(self):
+        self.width, self.height = unicorn.get_shape()
+        self.top = self.height - 1
+        self.right = self.width - 1
+        self.left = 0
+        self.bottom = 0
+        self.points = []
     rotation = 0
     brightness = 0.7
-    # calculated values
-    width, height = unicorn.get_shape()
-    left = 0
-    right = width - 1
-    top = height - 1
-    bottom = 0
-    points = []
 
 
 class GridController:
-    def __init__(self):
+    def __init__(self, grid):
         unicorn.set_layout(unicorn.AUTO)
-        unicorn.rotation(Grid.rotation)
-        unicorn.brightness(Grid.brightness)
+        unicorn.rotation(grid.rotation)
+        unicorn.brightness(grid.brightness)
 
-    def plot_points(self):
+    def plot_points(self, grid):
         unicorn.clear()
-        for point in Grid.points:
+        for point in grid.points:
             unicorn.set_pixel(point.x, point.y,
                               point.colour[0], point.colour[1], point.colour[2])
         unicorn.show()
 
-    def update_positions(self):
+    def update_positions(self, grid):
 
-        for point in Grid.points:
+        for point in grid.points:
             if point.direction == 0:
                 point.y += 1
             elif point.direction == 1:
@@ -47,31 +47,31 @@ class GridController:
                 point.y -= 1
             else:
                 point.x -= 1
-            self.validate_point(point)
+            self.validate_point(point, grid)
 
-    def validate_point(self, point):
+    def validate_point(self, point, grid):
 
-        if Grid.left < point.x > Grid.right:
-            Grid.points.remove(point)
-        if Grid.bottom < point.y > Grid.top:
-            Grid.points.remove(point)
+        if grid.left < point.x > grid.right:
+            grid.points.remove(point)
+        if grid.bottom < point.y > grid.top:
+            grid.points.remove(point)
 
 
 class Node:
-    def __init__(self):
+    def __init__(self, grid):
         direction = randint(0, 3)
         self.direction = direction
-        self.direction_coordinate_determination(direction)
+        self.direction_coordinate_determination(direction, grid)
         self.set_color()
 
-    def direction_coordinate_determination(self, index):
+    def direction_coordinate_determination(self, index, grid):
         switch = {
-            0: self.set_direction(self.get_dynamic_position(Grid.right), Grid.bottom),
-            1: self.set_direction(Grid.left, self.get_dynamic_position(Grid.top)),
-            2: self.set_direction(self.get_dynamic_position(Grid.right), Grid.top),
-            3: self.set_direction(Grid.right, self.get_dynamic_position(Grid.top)),
+            0: self.set_direction(self.get_dynamic_position(grid.right), grid.bottom),
+            1: self.set_direction(grid.left, self.get_dynamic_position(grid.top)),
+            2: self.set_direction(self.get_dynamic_position(grid.right), grid.top),
+            3: self.set_direction(grid.right, self.get_dynamic_position(grid.top)),
         }
-        return switch.get(index, (self.get_dynamic_position(Grid.width - 1), Grid.bottom))
+        return switch.get(index, (self.get_dynamic_position(grid.width - 1), grid.bottom))
 
     def get_dynamic_position(self, max):
         return randint(0, max)
@@ -89,10 +89,12 @@ print("""--BEGIN
     unicorn pi light sequence...ctrl + c to exit...
 """)
 
-controller = GridController()
+grid = Grid()
+controller = GridController(grid)
 
 while True:
-    Grid.points.append(Node())
-    controller.plot_points()
-    controller.update_positions()
+    if len(grid.points) <= Sequence.max_nodes:
+        grid.points.append(Node(grid))
+        controller.plot_points(grid)
+        controller.update_positions(grid)
     time.sleep(Sequence.delay)
